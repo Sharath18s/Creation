11f80081e5d014c073ba8fb0faa3fa173daa5e9a
Sharath18s <darlingsr72@gmail.com>
2021-12-16 16:05:20 +0530

    Base commit

---
+++ src/test/java/Account.java
@@ -0,0 +1,45 @@
+
+public class Account {
+
+
+
+
+
+			private long balance;
+			private String Accno;
+			
+			Account(int amt){
+				balance=amt;
+				
+				
+				
+			}
+			
+			public void withdraw(int amt) {
+				String msg="";
+				try {
+				balance=balance-amt;
+				msg="Transaction success";
+				} catch (Exception e){
+					msg="Failed try again";
+				}
+				System.out.println(msg);
+			}
+			public void deposit(int amt) {
+				String msg="";
+				try {
+				balance=balance+amt;
+				msg="Transaction success";
+				} catch (Exception e){
+					msg="Failed try again";
+				}
+				System.out.println(msg);
+		}
+
+			
+
+
+
+
+
+}
